c93fe72218b016b31cc2824732c13fccc7458f80
${long10280} <${1767667559@qq.com}>
2020-06-27 21:10:23 +0800

    尝试进行第二次提交

--- demo-module-1/src/com/duyi/MainTest.java
+++
@@ -1,7 +0,0 @@
-package com.duyi;
-
-public class MainTest {
-    public static void main(String[] args) {
-
-    }
-}
--- demo-module-1/src/com/duyi/MainTest1.java
+++
@@ -1,7 +0,0 @@
-package com.duyi;
-
-public class MainTest1 {
-    public static void main(String[] args) {
-        System.out.println("hello git");
-    }
-}
